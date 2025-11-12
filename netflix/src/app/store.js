20ecacbee184e6f2ce62ba880a3c2aeefee7c223
ndmit <n.dmitriymih@gmail.com>
2025-11-12 23:23:36 +0300

    slider dont work

--- netflix/src/app/store.js
+++ netflix/src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { currentQuerySlice as currentQueryReducer } from '../features/currentQuerySlice.js';
+import  currentQueryReducer  from '../features/currentQuerySlice.js';
 import { kinopoiskApi } from '../services/kinopoiskApi.js';
 
 export const store = configureStore({
   reducer: {
     [kinopoiskApi.reducerPath]: kinopoiskApi.reducer,
-    currentSlice: currentQueryReducer,
+    currentQuerySlice: currentQueryReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(kinopoiskApi.middleware),
--- netflix/src/components/pages/Movies/Movies.jsx
+++ netflix/src/components/pages/Movies/Movies.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-
-import useMoviesQuery from '../../../hooks/useMoviesQuery.jsx';
+import { Link, Stack } from '@mui/material';
 import BearCarousel, { BearSlideImage } from 'bear-react-carousel';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-const Movies = () => {
+import useMoviesQuery from '../../../hooks/useMoviesQuery';
+import ErrorMessage from '../../ui/ErrorMessage';
+import MoviesSkeleton from './MoviesSkeleton';
+
+export default function Movies() {
   const {
     isLoading,
     hasError,
     responsePopular,
-    // responseBest,
-    // responseFilms,
-    // responseSerials,
-    // responseCartoons,
+    responseBest,
+    responseFilms,
+    responseSerials,
+    responseCartoons,
   } = useMoviesQuery();
   
-  if (isLoading) return <p>Загрузка...</p>;
-  if (hasError) return <p>Ошибка...</p>;
+  if (isLoading) return <MoviesSkeleton />;
   
-  const serializeDataForCarousel = data => {
-    data.map((row) => (
-      <BearSlideImage key={row.id}>
-        {row.name}
-      </BearSlideImage>
+  if (hasError) return <ErrorMessage />;
+  
+  const serializeDataForCarousel = data =>
+    data.map(row => (
+      <RouterLink key={row.id} to={`/movie/${row.kinopoiskId}`}>
+        <BearSlideImage imageUrl={row.posterUrlPreview} />
+      </RouterLink>
     ));
-  };
+  
+  console.log(responsePopular.data.items[1].kinopoiskId)
+  console.log(responsePopular.data.items[1].posterUrlPreview)
   
   const carouselArr = [
     {
@@ -31,13 +38,59 @@ const Movies = () => {
       url: '/popular',
       data: serializeDataForCarousel(responsePopular.data.items),
     },
+    {
+      title: 'Лучшие фильмы',
+      url: '/best',
+      data: serializeDataForCarousel(responseBest.data.items),
+    },
+    {
+      title: 'Фильмы',
+      url: '/films',
+      data: serializeDataForCarousel(responseFilms.data.items),
+    },
+    {
+      title: 'Сериалы',
+      url: '/serials',
+      data: serializeDataForCarousel(responseSerials.data.items),
+    },
+    {
+      title: 'Мультфильмы',
+      url: '/cartoons',
+      data: serializeDataForCarousel(responseCartoons.data.items),
+    },
   ];
-  
+  //TODO 3 24
   return (
-    <div>
-      <BearCarousel data={carouselArr[0].data} />
-    </div>
+    <>
+      {carouselArr.map(carousel => (
+        <Stack key={carousel.title}>
+          <Link
+            sx={{ mt: 2, mb: 2 }}
+            variant="h4"
+            component={RouterLink}
+            to={carousel.url}
+          >
+            {carousel.title}
+          </Link>
+          <BearCarousel
+            data={carousel.data}
+            slidesPerView={1}
+            slidesPerGroup={1}
+            isEnableNavButton
+            isEnableLoop
+            isEnableAutoPlay
+            autoPlayTime={5000}
+            breakpoints={{
+              375: {
+                autoPlayTime: 0,
+              },
+              768: {
+                slidesPerView: 5,
+              },
+            }}
+          />
+        </Stack>
+      ))}
+    </>
   );
-};
-
-export default Movies;
+}
