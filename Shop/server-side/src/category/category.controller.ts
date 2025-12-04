16202ab1341d870a73f125135cea34d7c582a201
ndmit <n.dmitriymih@gmail.com>
2025-12-04 23:51:47 +0300

    ...

--- Shop/server-side/src/category/category.controller.ts
+++ Shop/server-side/src/category/category.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { categoryService } from './category.service'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { CreateStoreDto } from '../store/dto/create-store.dto'
+import { UpdateStoreDto } from '../store/dto/update-store.dto'
+import { categoryDto } from './dto/category.dto'
 
-@Controller('categories')
-export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
-} //TODO 1 40 15
+@Controller('category')
+export class categoryController {
+	constructor(private readonly categoryService: categoryService) {}
+	
+	@Auth()
+	@Get('by-storeId/:storeId')
+	async getByStoreId(@Param('storeId') storeId: string) {
+		return this.categoryService.getByStoreId(storeId)
+	}
+	
+	@Auth()
+	@Get('by-id/:id')
+	async getById(@Param('id') id: string) {
+		return this.categoryService.getById(id)
+	}
+	
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post(':storeId')
+	async create(@Param('storeId') id: string, @Body() dto: categoryDto) {
+		return this.categoryService.create(id, dto)
+	}
+	
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: categoryDto) {
+		return this.categoryService.update(id, dto)
+	}
+	
+	@HttpCode(200)
+	@Auth()
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.categoryService.delete(id)
+	}
+}
--- Shop/server-side/src/category/category.service.ts
+++ Shop/server-side/src/category/category.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { CreateStoreDto } from '../store/dto/create-store.dto'
+import { UpdateStoreDto } from '../store/dto/update-store.dto'
+import { CategoryDto } from './dto/Category.dto'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private prisma: PrismaService) {}
+	
+	async getByStoreId(stroreId: string) {
+		return this.prisma.category.findMany({
+			where: {
+				stroreId
+			}
+		})
+	}
+	
+	async getById(id: string) {
+		const category = await this.prisma.Category.findUnique({
+			where: {
+				id
+			}
+		})
+		
+		if (!category) {
+			throw new NotFoundException(`Категория не найдена`)
+		}
+		
+		return category
+	}
+	
+	async create(storeId: string, dto : CategoryDto) {
+		return this.prisma.Category.create({
+			data: {
+				title: dto.title,
+				description: dto.description,
+				storeId
+			}
+		})
+	}
+	
+	async update(id: string, dto: CategoryDto) {
+		await this.getById(id)
+		
+		return this.prisma.Category.update({
+			where: {
+				id
+			},
+			data: dto
+		})
+	}
+	
+	async delete(id: string) {
+		await this.getById(id)
+		
+		return this.prisma.Category.delete({
+			where: {
+				id
+			}
+		})
+	}
+}
