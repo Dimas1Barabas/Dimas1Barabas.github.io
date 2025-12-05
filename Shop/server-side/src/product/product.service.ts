e913f28e6efe6ce82195ef18c5597d512c8c8951
ndmit <n.dmitriymih@gmail.com>
2025-12-05 22:06:49 +0300

    fix1

--- Shop/server-side/src/product/product.service.ts
+++ Shop/server-side/src/product/product.service.ts
@@ -29,9 +29,95 @@ export class ProductService {
 					title: {
 						contains: searchTerm,
 						mode: 'insensitive'
+					},
+					description: {
+						contains: searchTerm,
+						mode: 'insensitive'
 					}
 				}
 			]
 		}
 	}
+	
+	async getByStoreId(stroreId: string) {
+		return this.prisma.product.findMany({
+			where: {
+				stroreId
+			},
+			include: {
+				category: true,
+				color: true
+			}
+		})
+	}
+	
+	async getById(id: string) {
+		const product = await this.prisma.product.findUnique({
+			where: {
+				id
+			},
+			include: {
+				category: true,
+				color: true,
+				reviews: true
+			}
+		})
+		
+		if (!product) {
+			throw new NotFoundException(`Товар не найден`)
+		}
+		
+		return product
+	}
+	
+	async getByCategory(categoryId: string) {
+		const products = await this.prisma.product.findMany({
+			where: {
+				category: {
+					id: categoryId
+				}
+			},
+			include: {
+				category: true,
+				color: true,
+				reviews: true
+			}
+		})
+		
+		if (!products) {
+			throw new NotFoundException(`Товары не найдены`)
+		}
+		
+		return products
+	}
+	
+	async getMostPopular() {
+		const getMostPopularProducts = await this.prisma.orderItem.groupBy({
+			by: ['productId'],
+			_count: {
+				id: true
+			},
+			orderBy: {
+				_count: {
+					id: 'desc'
+				}
+			}
+		})
+		
+		const productIds = getMostPopularProducts.map(item => item.productId)
+		
+		const products = await this.prisma.product.findMany({
+			where: {
+				id: {
+					in: productIds
+				}
+			},
+			include: {
+				category: true,
+			}
+		})
+		
+		return products
+	}
 }
+//TODO 1 57 48
