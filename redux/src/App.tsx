d19895ea141ca4cf0a8765db963081c2dbacc30a
ndmit <n.dmitriymih@gmail.com>
2025-12-30 01:03:48 +0300

    ...

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {store} from './store.ts';
+import {useEffect, useReducer} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [_, forceUpdate] = useReducer((x) => x + 1, 0)
+  
+  useEffect(() => {
+    const unsubsribe = store.subscribe(() => {
+      forceUpdate()
+    });
+    
+    return unsubsribe
+  }, []);
+  // TODO 45
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      counter {store.getState().counters}
+      <button onClick={() =>
+        store.dispatch({ type: 'increment' })
+      }>
+        Increment
+      </button>
+      <button onClick={() =>
+        store.dispatch({ type: 'decrement' })
+      }>
+        Decrement
+      </button>
+    </div>
   )
 }
 
