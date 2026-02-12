8ed6901b380908c01a8df14779b77c45ffba843e
ndmit <n.dmitriymih@gmail.com>
2026-02-12 17:13:13 +0300

    filters

--- pizza/app/page.tsx
+++ pizza/app/page.tsx
@@ -1,13 +1,35 @@
-import {Categories, Container, Title} from '@/components';
-import {SortPopup} from '@/components/shared/sort-popup';
+import {Container, Filters, Title, TopBar} from '@/components';
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size='lg' className="font-extrabold" />
-        <Categories />
-        <SortPopup />
+      </Container>
+      
+      <TopBar />
+      
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          
+          {/*<div className="flex-1">*/}
+          {/*  <div className="flex flex-col gap-16">*/}
+          {/*    <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />*/}
+          {/*    <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} />*/}
+          {/*  </div>*/}
+          {/*  */}
+          {/*  <div className="flex items-center gap-6 mt-12">*/}
+          {/*    <Pagination pageCount={3} />*/}
+          {/*    <span className="text-sm text-gray-400">5 из 65</span>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+          
+          
+        </div>
       </Container>
     </>
   );
--- pizza/components/shared/index.ts
+++ pizza/components/shared/index.ts
@@ -3,10 +3,11 @@ export { Header } from './header';
 export { Title } from './title';
 export { Categories } from './categories';
 export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export {RangeSlider} from './range-slider'
 // export { SearchInput } from './search-input';
-// export { TopBar } from './top-bar';
-// export { Filters } from './filters';
-// export { FilterCheckbox } from './filter-checkbox';
 // export { PizzaImage } from './pizza-image';
 // export { ChooseProductForm } from './choose-product-form';
 // export { ChoosePizzaForm } from './choose-pizza-form';
